9af63e76f6621127abab36c510317c6672766998
Tim Roberts <tim.roberts20@imperial.ac.uk>
2021-11-22 14:03:53 +0000

    monday progress

---
+++ quiz/quiz.py
@@ -0,0 +1,19 @@
+answer = input("Do you want to play? (type yes/no here)")
+
+if answer == "yes":
+    print("cool, lets play")
+
+if answer== "no":
+    print("well then you suck") 
+
+print("What is Conrad's favourite sport?")
+
+print("What's Nini's favourite sport?")
+
+print("What's Tim's favourite computer game?")
+
+print("What's Conrad's favourite book?")
+
+print("What's Nini's favourite song?")
+
+print("What's Tim's favourite vegetable?")
